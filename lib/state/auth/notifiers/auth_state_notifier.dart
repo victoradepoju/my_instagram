9ea02cb4597a0a44f866e4a4b44bc4c30cf2892a
victoradepoju <victoradepoju30@gmail.com>
2023-01-05 18:42:02 +0100

    firebs fields, userinfostorage, statenot and provs

--- lib/state/auth/notifiers/auth_state_notifier.dart
+++ lib/state/auth/notifiers/auth_state_notifier.dart
@@ -2,9 +2,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/backend/authenticator.dart';
 import 'package:instagram_clone/state/auth/models/auth_result.dart';
 import 'package:instagram_clone/state/auth/models/auth_state.dart';
+import 'package:instagram_clone/state/posts/typedefs/user_id.dart';
+import 'package:instagram_clone/state/user_info/backend/user_info_storage.dart';
 
 class AuthStateNotifier extends StateNotifier<AuthState> {
   final _authenticator = const Authenticator();
+  final _userInfoStorage = const UserInfoStorage();
 
   AuthStateNotifier() : super(const AuthState.unknown()) {
     if (_authenticator.isAlreadyLoggedIn) {
@@ -26,6 +29,40 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
     state = state.copyWithIsLoading(true);
     final result = await _authenticator.loginWithGoogle();
     final userId = _authenticator.userId;
-    if (result == AuthResult.success && userId != null) {}
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(
+        userId: userId,
+      );
+    }
+    state = AuthState(
+      result: result,
+      isLoading: false,
+      userId: userId,
+    );
   }
+
+  Future<void> loginWithFacebook() async {
+    state = state.copyWithIsLoading(true);
+    final result = await _authenticator.loginWithFacebook();
+    final userId = _authenticator.userId;
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(
+        userId: userId,
+      );
+    }
+    state = AuthState(
+      result: result,
+      isLoading: false,
+      userId: userId,
+    );
+  }
+
+  Future<void> saveUserInfo({required UserId userId}) =>
+      _userInfoStorage.saveUserInfo(
+        // we are not using the userId from _authenticator because it is
+        // optional (nullable)
+        userId: userId,
+        displayName: _authenticator.displayName,
+        email: _authenticator.email,
+      );
 }
---
+++ lib/state/user_info/models/user_info_payload.dart
@@ -0,0 +1,21 @@
+import 'dart:collection' show MapView;
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:instagram_clone/state/constants/firebase_field_name.dart';
+import 'package:instagram_clone/state/posts/typedefs/user_id.dart';
+
+@immutable
+// using a MapView so it can be easily added to Firebase.
+// as long as you extend a MapView then your object is a map.
+class UserInfoPayload extends MapView<String, String> {
+  UserInfoPayload({
+    required UserId userId,
+    required String? displayName,
+    required String? email,
+  }) : super(
+          {
+            FirebaseFieldName.userId: userId,
+            FirebaseFieldName.displayName: displayName ?? '',
+            FirebaseFieldName.email: email ?? '',
+          },
+        );
+}

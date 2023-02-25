d97e2cf520efbfeb09f90c53eaad78f50a18fe2b
victoradepoju <victoradepoju30@gmail.com>
2023-02-25 17:55:13 +0100

    image picker helper class

--- lib/state/image_upload/extensions/to_file.dart
+++ lib/state/image_upload/extensions/to_file.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:image_picker/image_picker.dart';
 
+// This is because The image_picker package returns gallery images
+// in XFile format
+
 extension ToFile on Future<XFile?> {
   Future<File?> toFile() => then((xFile) => xFile?.path).then(
         (filePath) => filePath != null ? File(filePath) : null,

84824707787e39c9289b8a7ddd5b9d6e25ccbb70
victoradepoju <victoradepoju30@gmail.com>
2023-02-25 18:53:40 +0100

    image and video upload actions

--- lib/state/providers/is_loading_provider.dart
+++ lib/state/providers/is_loading_provider.dart
@@ -1,8 +1,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
+import 'package:instagram_clone/state/image_upload/providers/image_uploader_provider.dart';
 
 final isLoadingProvider = Provider<bool>((ref) {
   final authState = ref.watch(authStateProvider);
+  final isUploadingImage = ref.watch(imageUploaderProvider);
 
-  return authState.isLoading;
+  return authState.isLoading || isUploadingImage;
 });
--- lib/views/main/main_view.dart
+++ lib/views/main/main_view.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
+import 'package:instagram_clone/state/image_upload/helpers/image_picker_helper.dart';
+import 'package:instagram_clone/state/image_upload/models/file_type.dart';
+import 'package:instagram_clone/state/post_settings/providers/post_settings_provider.dart';
 import 'package:instagram_clone/views/components/dialogs/alert_dialog_model.dart';
 import 'package:instagram_clone/views/components/dialogs/logout_dialog.dart';
 import 'package:instagram_clone/views/constants/strings.dart';
+import 'package:instagram_clone/views/create_new_post/create_new_post_view.dart';
 import 'package:instagram_clone/views/tabs/users_posts/user_posts_view.dart';
 
 class MainView extends ConsumerStatefulWidget {
@@ -26,13 +30,63 @@ class _MainViewState extends ConsumerState<MainView> {
           ),
           actions: [
             IconButton(
-              onPressed: () async {},
+              onPressed: () async {
+                // pick video first
+                final videoFile =
+                    await ImagePickerHelper.pickVideoFromGallery();
+                if (videoFile == null) {
+                  return;
+                }
+
+                // so the post setting doesn't get stored
+                ref.refresh(postSettingsProvider);
+
+                // go to the screen to create a new post
+                if (!mounted) {
+                  return;
+                }
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CreateNewPostView(
+                      fileToPost: videoFile,
+                      fileType: FileType.video,
+                    ),
+                  ),
+                );
+              },
               icon: const FaIcon(
                 FontAwesomeIcons.film,
               ),
             ),
             IconButton(
-              onPressed: () async {},
+              onPressed: () async {
+                // pick an image first
+                final imageFile =
+                    await ImagePickerHelper.pickImageFromGallery();
+                if (imageFile == null) {
+                  return;
+                }
+
+                // so the post setting doesn't get stored
+                ref.refresh(postSettingsProvider);
+
+                // go to the screen to create a new post
+                if (!mounted) {
+                  return;
+                }
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CreateNewPostView(
+                      fileToPost: imageFile,
+                      fileType: FileType.image,
+                    ),
+                  ),
+                );
+              },
               icon: const Icon(
                 Icons.add_photo_alternate_outlined,
               ),

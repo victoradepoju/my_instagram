f44db596ba7082abe222cbaac21be824e03126b3
victoradepoju <victoradepoju30@gmail.com>
2023-02-28 21:38:06 +0100

    comment payload done

--- lib/state/comments/models/comment_payload.dart
+++ lib/state/comments/models/comment_payload.dart
@@ -1,4 +1,23 @@
+import 'dart:collection' show MapView;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart' show immutable;
+import 'package:instagram_clone/state/constants/firebase_field_name.dart';
+import 'package:instagram_clone/state/posts/typedefs/post_id.dart';
+import 'package:instagram_clone/state/posts/typedefs/user_id.dart';
 
 @immutable
-class CommentPaload {}
+class CommentPayload extends MapView<String, dynamic> {
+  CommentPayload({
+    required UserId fromUserId,
+    required PostId onPostId,
+    required String comment,
+  }) : super(
+          {
+            FirebaseFieldName.userId: fromUserId,
+            FirebaseFieldName.postId: onPostId,
+            FirebaseFieldName.comment: comment,
+            FirebaseFieldName.createdAt: FieldValue.serverTimestamp(),
+          },
+        );
+}

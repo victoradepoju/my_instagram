c2bc9a3c43616450176030359c069b6d7a45b6f1
victoradepoju <victoradepoju30@gmail.com>
2023-03-03 23:30:31 +0100

    post video view done

---
+++ lib/views/components/post/post_video_view.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:instagram_clone/state/posts/models/post.dart';
+import 'package:instagram_clone/views/components/animations/error_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/loading_animation_view.dart';
+import 'package:video_player/video_player.dart';
+
+class PostVideoView extends HookWidget {
+  final Post post;
+  const PostVideoView({
+    super.key,
+    required this.post,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = VideoPlayerController.network(
+      post.fileUrl,
+    );
+
+    final isVideoPlayerReady = useState(false);
+
+    useEffect(() {
+      controller.initialize().then(
+        (value) {
+          isVideoPlayerReady.value = true;
+          controller.setLooping(true);
+          controller.play();
+        },
+      );
+      return controller.dispose;
+    }, [controller]);
+
+    switch (isVideoPlayerReady.value) {
+      case true:
+        return AspectRatio(
+          aspectRatio: post.aspectRatio,
+          child: VideoPlayer(
+            controller,
+          ),
+        );
+      case false:
+        return const LoadingAnimatonView();
+      default:
+        // this shouldn't be called
+        return const ErrorAnimatonView();
+    }
+  }
+}

292ce75ca0805ee30c6ec1cac6c7fca9b12605a1
victoradepoju <victoradepoju30@gmail.com>
2023-03-04 06:54:48 +0100

    post likes count provider done

---
+++ lib/state/likes/providers/post_likes_count_provider.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/constants/firebase_collection_name.dart';
+import 'package:instagram_clone/state/constants/firebase_field_name.dart';
+import 'package:instagram_clone/state/posts/typedefs/post_id.dart';
+
+final postLikesCountProvider = StreamProvider.family<int, PostId>((
+  ref,
+  PostId postId,
+) {
+  final controller = StreamController<int>.broadcast();
+
+  // by defauld, 0 people have liked a post
+  controller.onListen = () {
+    controller.sink.add(0);
+  };
+
+  final sub = FirebaseFirestore.instance
+      .collection(FirebaseCollectionName.likes)
+      .where(
+        FirebaseFieldName.postId,
+        isEqualTo: postId,
+      )
+      .snapshots()
+      .listen(
+    (snapshot) {
+      controller.sink.add(
+        snapshot.docs.length,
+      );
+    },
+  );
+
+  ref.onDispose(() {
+    sub.cancel();
+    controller.close();
+  });
+
+  return controller.stream;
+});

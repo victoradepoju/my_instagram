99522edd515b42555bf080dadc27d08f098306d7
victoradepoju <victoradepoju30@gmail.com>
2023-03-04 06:07:30 +0100

    rich text with two parts done

---
+++ lib/views/components/rich_two_parts_text.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class RichTwoPartsText extends StatelessWidget {
+  final String leftPart;
+  final String rightPart;
+  const RichTwoPartsText({
+    Key? key,
+    required this.leftPart,
+    required this.rightPart,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        style: const TextStyle(
+          color: Colors.white70,
+          height: 1.5,
+        ),
+        children: [
+          TextSpan(
+            text: leftPart,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextSpan(
+            text: ' $rightPart',
+          ),
+        ],
+      ),
+    );
+  }
+}

9456fe40b7d9d99b6ad36d21560536f0f9b95589
victoradepoju <victoradepoju30@gmail.com>
2023-03-04 20:26:22 +0100

    isloading provider modified, but a bug with post deletion

--- lib/state/providers/is_loading_provider.dart
+++ lib/state/providers/is_loading_provider.dart
@@ -1,10 +1,20 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
+import 'package:instagram_clone/state/comments/providers/delete_comment_provider.dart';
+import 'package:instagram_clone/state/comments/providers/send_comment_provider.dart';
 import 'package:instagram_clone/state/image_upload/providers/image_uploader_provider.dart';
+import 'package:instagram_clone/state/posts/providers/delete_post_provider.dart';
 
 final isLoadingProvider = Provider<bool>((ref) {
   final authState = ref.watch(authStateProvider);
   final isUploadingImage = ref.watch(imageUploaderProvider);
+  final isSendingComment = ref.watch(sendCommentProvider);
+  final isDeletingComment = ref.watch(deleteCommentProvider);
+  final isDeletingPost = ref.watch(deletePostProvider);
 
-  return authState.isLoading || isUploadingImage;
+  return authState.isLoading ||
+      isUploadingImage ||
+      isSendingComment ||
+      isDeletingPost ||
+      isDeletingComment;
 });

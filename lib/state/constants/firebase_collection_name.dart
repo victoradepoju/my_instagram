3c1520ea468210a1b03f8f6bca24ba24e59f95e4
victoradepoju <victoradepoju30@gmail.com>
2023-03-04 07:20:52 +0100

    like model as mapview for easy serialization

--- lib/state/constants/firebase_collection_name.dart
+++ lib/state/constants/firebase_collection_name.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart' show immutable;
 class FirebaseCollectionName {
   static const thumbnails = 'thumbnails';
   static const comments = 'comments';
-  static const like = 'likes';
+  static const likes = 'likes';
   static const posts = 'posts';
   static const users = 'users';
 
---
+++ lib/state/likes/models/like.dart
@@ -0,0 +1,21 @@
+import 'dart:collection' show MapView;
+
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:instagram_clone/state/constants/firebase_field_name.dart';
+import 'package:instagram_clone/state/posts/typedefs/post_id.dart';
+import 'package:instagram_clone/state/posts/typedefs/user_id.dart';
+
+@immutable
+class Like extends MapView<String, String> {
+  Like({
+    required PostId postId,
+    required UserId likedBy,
+    required DateTime date,
+  }) : super(
+          {
+            FirebaseFieldName.postId: postId,
+            FirebaseFieldName.userId: likedBy,
+            FirebaseFieldName.date: date.toIso8601String(),
+          },
+        );
+}

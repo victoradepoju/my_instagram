fa243bee2e4aa428b97701632fce0cb08d8c6996
victoradepoju <victoradepoju30@gmail.com>
2023-03-05 14:29:55 +0100

    search view done and attached to mainview

--- lib/views/main/main_view.dart
+++ lib/views/main/main_view.dart
@@ -9,6 +9,7 @@ import 'package:instagram_clone/views/components/dialogs/alert_dialog_model.dart
 import 'package:instagram_clone/views/components/dialogs/logout_dialog.dart';
 import 'package:instagram_clone/views/constants/strings.dart';
 import 'package:instagram_clone/views/create_new_post/create_new_post_view.dart';
+import 'package:instagram_clone/views/tabs/search/search_view.dart';
 import 'package:instagram_clone/views/tabs/users_posts/user_posts_view.dart';
 
 class MainView extends ConsumerStatefulWidget {
@@ -130,7 +131,7 @@ class _MainViewState extends ConsumerState<MainView> {
         body: const TabBarView(
           children: [
             UserPostsView(),
-            UserPostsView(),
+            SearchView(),
             UserPostsView(),
           ],
         ),

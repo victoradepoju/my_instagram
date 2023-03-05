77f642ffce91a7dd4c54c9a82c4a15f35a068803
victoradepoju <victoradepoju30@gmail.com>
2023-03-05 15:15:56 +0100

    home view done

---
+++ lib/views/tabs/home/home_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/posts/providers/all_posts_provider.dart';
+import 'package:instagram_clone/views/components/animations/empty_contents_with_text_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/error_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/loading_animation_view.dart';
+import 'package:instagram_clone/views/components/post/posts_grid_view.dart';
+import 'package:instagram_clone/views/constants/strings.dart';
+
+class HomeView extends ConsumerWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final posts = ref.watch(allPostsProvider);
+    return RefreshIndicator(
+      onRefresh: () {
+        ref.refresh(allPostsProvider);
+        return Future.delayed(
+          const Duration(seconds: 1),
+        );
+      },
+      child: posts.when(
+        data: (posts) {
+          if (posts.isEmpty) {
+            return const EmptyContentsWithTextAnimationView(
+              text: Strings.noPostsAvailable,
+            );
+          } else {
+            return PostsGridView(
+              posts: posts,
+            );
+          }
+        },
+        error: (error, stackTrace) {
+          return const ErrorAnimationView();
+        },
+        loading: () {
+          return const LoadingAnimatonView();
+        },
+      ),
+    );
+  }
+}
